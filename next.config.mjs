2068d9c787b44f0fe576da41038116c33d60eb87
Mads Smed <msth@trifork.com>
2022-10-13 22:35:52 +0200

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -21,5 +21,9 @@ export default defineNextConfig({
     locales: ["en"],
     defaultLocale: "en",
   },
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 });
